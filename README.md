f05ac570fc1ea498bb6a1daf1bc9d46f3031bdb0
zieleniowy <m.rynkowski@zhr.pl>
2021-06-01 21:29:58 +0200

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# next-aws-deploy
+# s3-deploy-script
+This module was developed to fasten deployment of static hosted websites into amazon s3 buckets.
+
+## install
+
+`
+npm install s3-deploy-script --save-dev
+`
+
+Make sure you add --save-dev switch to prevent this module from being loaded into production. It'd be just a waste of resources.
+
+## settings
+
+
+
+
+First of all you need to add this module as a script into your **package.json**.
+`
+...
+"scripts": {
+    ...
+    "deploy": "s3-deploy  --local-dir /public --bucket-name next-aws-deploy --max-age 86400
+    ...
+}
+...
+`
+
+(you can name it "deploy" as shown in the example or give it any other name you want)
+There are 3 switches:  
+- **--local-dir** - REQUIRED - directory path in which are your build files located (typically: "/public" or "/out")
+- **--bucket-name** - REQUIRED - name of your s3 bucket into which you want to upload your files
+- **--max-age** - OPTIONAL - max-age setting of Cache-Control header of your files. (how many seconds your files can be stored at the cache). Althought it's not necessery, it's strongly recommended to set this for performance reason. 
+
+Then you need to create a file called **aws.config.json**
+{ "accessKeyId": "YOUR_USER_ACCESS_KEY", "secretAccessKey": "YOUR_USER_SECRET_ACCESS_KEY", "region": "YOUR_S3_BUCKET_REGION" }
+this is file you may be familiar with because this json schema is fetched from aws documentation.
+
+### warning
+Keep in mind you need to set appropriate privileges for user whose accesskeys you're filling in **aws.config.json** to manage
+a s3 bucket you're provided in **package.json**. To manage users and roles type IAM in aws page search bar.
+
+
+
+## usage
+If you well configured your **package.json** and **aws.config.json** files you can simply execute **npm deploy** in your terminal.
--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+#!/usr/bin/env node
 const { defaultProvider } = require("@aws-sdk/credential-provider-node");
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, PutObjectAclCommand } = require("@aws-sdk/client-s3");
 const getFiles = require('./getFilesRecusive');
 const fs = require('fs');
+const path = require('path');
 
-const localDir = process.argv[2];
-const bucketName = process.argv[3];
+const args = Array.from(process.argv.slice(2));
+const localDir = args[args.indexOf('--local-dir')+1];
+const bucketName = args[args.indexOf('--bucket-name')+1];
+const maxAge = args.indexOf('--max-age')!==-1?args[args.indexOf('--max-age')+1]:undefined;
 
-
+if(!localDir || !bucketName){ throw new Error('required parameters not specified: --local-dir || --bucket-name '); }
 
 const provider = defaultProvider({
-    configFilepath: './aws.config.json'
+    configFilepath: path.join(__dirname, '../../', 'aws.config.json')
 });
 
+console.log(path.join(__dirname, '../../'));
 const client = new S3Client({ credentialDefaultProvider: provider });
   
 const run = async () => {
     console.log('Files are about to upload into AWS S3');
-    for await (const f of getFiles('./out')) {
+    for await (const f of getFiles(path.join(__dirname, '../../', localDir))) {
+        const Key = f.slice(f.match(localDir).index+localDir.length+1);
         client.send(new PutObjectCommand({
             Bucket: bucketName,
-            Key: f.slice(f.match('/out/').index+5),
-            Body: fileStream = fs.createReadStream(f)
+            Key,
+            Body: fileStream = fs.createReadStream(f),
+            ACL: 'public-read',
+            CacheControl: maxAge?`max-age=${maxAge}`:undefined
+
         })).then(()=>{
             process.stdout.clearLine(); 
             process.stdout.cursorTo(0); 
